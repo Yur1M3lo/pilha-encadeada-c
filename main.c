682cea58fdc7de43f18c78dff02c8d8949228503
Yur1M3lo <yurimelo@ufrj.br>
2023-07-17 13:59:34 -0300

    adicionando metodos de uma pilha encadeada

---
+++ main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct No
+{
+    int info;
+    struct No* proximo;
+};
+
+struct Pilha
+{
+    struct No* topo;
+    int tamanho;
+};
+
+struct Pilha* criar()
+{
+    struct Pilha* nova_pilha = (struct Pilha*) malloc(sizeof(struct Pilha));
+    if(nova_pilha != NULL)
+    {
+        nova_pilha->topo = NULL;
+        nova_pilha->tamanho = 0;
+    }
+    return nova_pilha;
+}
+
+void empilhar(struct Pilha* p, int item)
+{
+    assert(p != NULL);
+    struct No* novo_no = (struct No*) malloc(sizeof(struct No));
+    if(novo_no != NULL)
+    {
+        novo_no->info = item;
+        novo_no->proximo = p->topo;
+        p->topo = novo_no;
+        p->tamanho++;
+    }
+}
+
+void desempilhar(struct Pilha* p)
+{
+    assert(p != NULL);
+    assert(p->topo != NULL);
+    struct No* aux = p->topo;
+    int elemento = aux->info;
+    p->topo = aux->proximo;
+    p->tamanho--;
+    free(aux);
+    return elemento;
+}
+
+int topo(struct Pilha* p)
+{
+    assert(p != NULL);
+    assert(p->topo != NULL);
+    struct No* topo = p->topo;
+    return topo->info;
+}
+
+int tamanho(struct Pilha* p)
+{
+    assert(p != NULL);
+    return p->tamanho;
+}
+
+bool vazia(struct Pilha* p)
+{
+    assert(p != NULL);
+    return (p->topo == NULL);
+}
+
+void liberar(struct Pilha* p)
+{
+    assert(p != NULL);
+    while (vazia(p) == false)
+    {
+        desempilhar(p);
+    }
+    free(p);
+}
+
+int main(int argc, char const *argv[])
+{   
+    struct Pilha* pilha = criar();
+    empilhar(pilha, 1);
+    empilhar(pilha, 2);
+    empilhar(pilha, 3);
+
+    return 0;
+}
